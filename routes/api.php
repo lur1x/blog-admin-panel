2a6629d00100d17f827568ae3032d04100f9fd68
Dmitry Kirillov <mr.lurix@gmail.com>
2026-09-13 11:11:40 +0300

    feat: register protected posts API routes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::get('/posts', [PostController::class, 'index']);
+    Route::get('/my-posts', [PostController::class, 'myPosts']);
 });
