356542d7df3dcc800d1426c82b6d107a6e54c31f
Dmitry Kirillov <mr.lurix@gmail.com>
2026-09-13 13:17:00 +0300

    chore: add generic type hints to factories for better IDE support

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,17 +2,23 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends Factory<Post>
+ */
 class PostFactory extends Factory
 {
+    protected $model = Post::class;
+
     public function definition(): array
     {
         return [
             'user_id' => User::factory(),
-            'title' => $this->faker->sentence(6),
-            'text' => $this->faker->paragraphs(3, true),
+            'title' => fake()->sentence(6),
+            'text' => fake()->paragraphs(3, true),
         ];
     }
 }
